2f0f8995c3ddbcfbec1c3f6e61cc99d8d8bd8672
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-18 15:04:27 +0100

    style: improve player

--- components/player.tsx
+++ components/player.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { Pause, Play } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
 import { Song } from '../utils/types';
 
 type T_Props = {
@@ -8,15 +10,29 @@ type T_Props = {
 };
 
 const Player = ({ song }: T_Props) => {
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	const togglePlay = () => {
+		setIsPlaying((prev) => !prev);
+	};
+
 	return (
-		<div className='bg-neutral-200 p-4'>
-			<h2 className='text-lg font-bold'>Player</h2>
-			<div className='flex flex-col gap-4'>
-				<Image src={song.cover} alt={song.title} width={200} height={200} />
-				<div className='flex flex-col gap-4'>
-					<button>Play/Pause</button>
-					<input type='range' min='0' max='100' />
-				</div>
+		<div className='flex max-w-md flex-col rounded-xl bg-white p-4'>
+			<Image
+				src={song.cover}
+				alt={song.title}
+				width={200}
+				height={200}
+				className='w-full rounded-xl'
+			/>
+			<h2 className='mx-2 mt-4 text-xl font-bold'>{song.title}</h2>
+			<div className='mt-4 flex gap-4 rounded-xl bg-neutral-100 p-4'>
+				<button
+					className='grid size-12 cursor-pointer place-content-center rounded-full bg-neutral-100 hover:bg-neutral-200'
+					onClick={togglePlay}>
+					{isPlaying ? <Pause /> : <Play />}
+				</button>
+				<input className='flex-1' type='range' min='0' max='100' />
 			</div>
 		</div>
 	);

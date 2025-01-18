c703760a9d79ee7c9e3d2bd5732a66331e31552d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-18 15:53:56 +0100

    feat: playing music in the Player

--- components/player.tsx
+++ components/player.tsx
@@ -2,7 +2,7 @@
 
 import { Pause, Play } from 'lucide-react';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { API_URL } from '../utils/api';
 import { Song } from '../utils/types';
 
@@ -12,10 +12,60 @@ type T_Props = {
 
 const Player = ({ song }: T_Props) => {
 	const [isPlaying, setIsPlaying] = useState(false);
+	const [progress, setProgress] = useState(0);
+	const audioRef = useRef<HTMLAudioElement>(null!);
 
 	const togglePlay = () => {
-		setIsPlaying((prev) => !prev);
+		if (audioRef.current) {
+			if (isPlaying) {
+				audioRef.current.pause();
+			} else {
+				audioRef.current.play();
+			}
+			setIsPlaying(!isPlaying);
+		}
 	};
+
+	// Update the progress bar when the song is playing
+	const handleTimeUpdate = () => {
+		if (audioRef.current) {
+			const currentTime = audioRef.current.currentTime;
+			const duration = audioRef.current.duration;
+			setProgress((currentTime / duration) * 100);
+		}
+	};
+
+	// Update the song progress when the user changes the progress bar
+	const handleProgressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (audioRef.current) {
+			const newProgress = Number(event.target.value);
+			audioRef.current.currentTime = (audioRef.current.duration * newProgress) / 100;
+			setProgress(newProgress);
+		}
+	};
+
+	// Update the progress bar when the song is playing
+	useEffect(() => {
+		const audioElement = audioRef.current;
+		if (audioElement) {
+			audioElement.addEventListener('timeupdate', handleTimeUpdate);
+		}
+		return () => {
+			if (audioElement) {
+				audioElement.removeEventListener('timeupdate', handleTimeUpdate);
+			}
+		};
+	}, []);
+
+	// Reset the player when the song changes
+	useEffect(() => {
+		setIsPlaying(false);
+		setProgress(0);
+		if (audioRef.current) {
+			audioRef.current.pause();
+			audioRef.current.currentTime = 0;
+		}
+	}, [song]);
 
 	return (
 		<div className='group flex max-w-md flex-col rounded-xl bg-white p-4'>
@@ -33,8 +83,16 @@ const Player = ({ song }: T_Props) => {
 					onClick={togglePlay}>
 					{isPlaying ? <Pause /> : <Play />}
 				</button>
-				<input className='flex-1' type='range' min='0' max='100' />
+				<input
+					className='flex-1'
+					type='range'
+					min='0'
+					max='100'
+					value={progress}
+					onChange={handleProgressChange}
+				/>
 			</div>
+			<audio ref={audioRef} src={`${API_URL}/${song.audio}`} />
 		</div>
 	);
 };

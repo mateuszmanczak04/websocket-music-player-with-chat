db087667665a98198f4002e991e6cc832246614f
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-18 13:37:42 +0100

    feat: create backend for songs

---
+++ backend/controllers/songController.ts
@@ -0,0 +1,101 @@
+import type { Request, Response } from 'express';
+import db from '../prisma/database';
+
+export const getAllSongs = async (_req: Request, res: Response): Promise<void> => {
+	try {
+		const songs = await db.song.findMany();
+		res.status(200).json(songs);
+	} catch {
+		res.status(500).json({ message: 'Server error' });
+	}
+};
+
+export const getSongById = async (req: Request, res: Response): Promise<void> => {
+	if (!req.params.id) {
+		res.status(400).json({ message: 'Song ID is required' });
+		return;
+	}
+
+	try {
+		const song = await db.song.findUnique({ where: { id: req.params.id } });
+		if (!song) {
+			res.status(404).json({ message: 'Song not found' });
+			return;
+		}
+
+		res.status(200).json(song);
+	} catch {
+		res.status(500).json({ message: 'Server error' });
+	}
+};
+
+// Create a new song
+export const createSong = async (req: Request, res: Response): Promise<void> => {
+	const { title, audio, cover } = req.body;
+
+	if (
+		!title ||
+		title.trim().length === 0 ||
+		!audio ||
+		audio.trim().length === 0 ||
+		!cover ||
+		cover.trim().length === 0
+	) {
+		res.status(400).json({ message: 'Bad request' });
+		return;
+	}
+
+	try {
+		const newSong = await db.song.create({ data: { title, audio, cover } });
+
+		res.status(201).json(newSong);
+	} catch {
+		res.status(500).json({ message: 'Server error' });
+	}
+};
+
+// Update an existing song
+export const updateSong = async (req: Request, res: Response): Promise<void> => {
+	if (!req.params.id) {
+		res.status(400).json({ message: 'Song ID is required' });
+		return;
+	}
+
+	try {
+		const updatedSong = await db.song.update({
+			where: { id: req.params.id },
+			data: { title: req.body.title, audio: req.body.audio, cover: req.body.cover },
+		});
+
+		if (!updatedSong) {
+			res.status(404).json({ message: 'Song not found' });
+			return;
+		}
+
+		res.status(200).json(updatedSong);
+	} catch {
+		res.status(500).json({ message: 'Server error' });
+	}
+};
+
+// Delete a song
+export const deleteSong = async (req: Request, res: Response): Promise<void> => {
+	if (!req.params.id) {
+		res.status(400).json({ message: 'Song ID is required' });
+		return;
+	}
+
+	try {
+		const song = await db.song.findUnique({ where: { id: req.params.id } });
+
+		if (!song) {
+			res.status(404).json({ error: 'Song not found' });
+			return;
+		}
+
+		await db.song.delete({ where: { id: req.params.id } });
+		res.status(200).json({ message: 'Song deleted successfully' });
+	} catch {
+		res.status(500).json({ message: 'Server error' });
+	}
+};
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,24 @@
 import type { Request, Response } from 'express';
 import express from 'express';
+import {
+	createSong,
+	deleteSong,
+	getAllSongs,
+	getSongById,
+	updateSong,
+} from './controllers/songController';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/songs', getAllSongs);
+app.get('/songs/:id', getSongById);
+app.post('/songs', createSong);
+app.put('/songs/:id', updateSong);
+app.delete('/songs/:id', deleteSong);
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello, world!');

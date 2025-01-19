fc7a65cc57b2f9cb3b29930735f8795c404a677d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-19 14:48:00 +0100

    fix: order messages by createdAt

--- backend/controllers/messageController.ts
+++ backend/controllers/messageController.ts
@@ -3,7 +3,11 @@ import db from '../prisma/database';
 
 export const getMessages = async (req: Request, res: Response): Promise<void> => {
 	try {
-		const messages = await db.message.findMany({});
+		const messages = await db.message.findMany({
+			orderBy: {
+				createdAt: 'asc',
+			},
+		});
 
 		if (!messages) {
 			res.status(404).json({ message: 'No messages found' });

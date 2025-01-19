bb6ac2ffbcf29e1b5da9b9fc3645e2464938af78
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-19 16:21:18 +0100

    style: move connected users to chat

--- components/app.tsx
+++ components/app.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Chat from './chat';
-import ConnectedUsers from './connected-users';
 import Player from './player';
 import TrackList from './track-list';
 import UploadForm from './upload-form';
@@ -15,7 +14,6 @@ const App = () => {
 			</nav>
 			<main className='grow-1 shrink-1 flex basis-full flex-col bg-white p-8'>
 				<h1 className='text-3xl font-bold'>Spotify Clone</h1>
-				<ConnectedUsers />
 				<Player />
 			</main>
 			<aside className='shrink-0 grow-0 basis-96 bg-neutral-100 p-8'>
--- components/connected-users.tsx
+++ components/connected-users.tsx
@@ -6,18 +6,17 @@ const ConnectedUsers = () => {
 	const { users, user } = useAppContext();
 
 	return (
-		<div className='mt-4 rounded-xl bg-neutral-100 p-6'>
-			<h2 className='text-lg font-bold'>Connected users</h2>
-
-			<ul className='mt-2 flex flex-wrap gap-x-2'>
-				{users.map((u) => (
-					<li key={u.id} className='rounded-full bg-white px-3 py-1'>
-						{u.username}
-						{user && u.id === user.id && ' (You)'}
-					</li>
-				))}
-			</ul>
-		</div>
+		<ul className='mt-4 flex flex-wrap gap-x-2 border-b border-neutral-300 pb-4'>
+			{users.map((u) => (
+				<li
+					key={u.id}
+					className='flex items-center rounded-full bg-green-100 px-3 py-1 text-sm'>
+					<div className='mr-2 size-3 rounded-full border border-green-400 bg-green-300'></div>
+					{u.username}
+					{user && u.id === user.id && ' (You)'}
+				</li>
+			))}
+		</ul>
 	);
 };
 

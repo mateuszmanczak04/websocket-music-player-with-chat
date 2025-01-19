3440a57ec7b71e27e90ea262b1d134ef0e3c6494
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-19 16:02:06 +0100

    feat: show message about new user in the chat

--- components/chat.tsx
+++ components/chat.tsx
@@ -5,8 +5,9 @@ import { useAppContext } from '../context/app-context';
 import { decryptMessage, encryptMessage } from '../utils/encryption';
 
 const Chat = () => {
-	const { messages, socket, user, groupKey } = useAppContext();
+	const { messages, socket, user, groupKey, users } = useAppContext();
 	const messagesRef = useRef<HTMLDivElement>(null!);
+	const previousUsers = useRef(users);
 
 	const handleSubmit = (e: React.FormEvent) => {
 		if (!user) return;
@@ -17,6 +18,23 @@ const Chat = () => {
 		socket.emit('send-message', encryptMessage(message, groupKey), user.username);
 		(e.target as HTMLFormElement).reset();
 	};
+
+	useEffect(() => {
+		if (previousUsers.current.length < users.length) {
+			const newUser = users.find(
+				(u) => !previousUsers.current.map((u) => u.id).includes(u.id),
+			);
+			if (newUser) {
+				socket.emit(
+					'send-message',
+					encryptMessage(`${newUser.username} joined the chat`, groupKey),
+					'[SERVER]',
+				);
+			}
+		}
+
+		previousUsers.current = users;
+	}, [users, groupKey, socket]);
 
 	useEffect(() => {
 		if (!messagesRef.current) return;
@@ -36,9 +54,22 @@ const Chat = () => {
 						style={{
 							alignSelf:
 								message.username === user?.username ? 'flex-end' : 'flex-start',
-							backgroundColor:
-								message.username === user?.username ? '#3b82f6' : '#f5f5f5',
+							backgroundColor: (() => {
+								switch (message.username) {
+									case '[SERVER]':
+										return 'transparent';
+									case user?.username:
+										return '#3b82f6';
+									default:
+										return '#f5f5f5';
+								}
+							})(),
 							color: message.username === user?.username ? 'white' : '#262626',
+							width: message.username === '[SERVER]' ? '100%' : 'fit-content',
+							fontSize: message.username === '[SERVER]' ? '0.8rem' : '1rem',
+							textAlign: message.username === '[SERVER]' ? 'center' : 'left',
+							display: message.username === '[SERVER]' ? 'flex' : 'block',
+							gap: message.username === '[SERVER]' ? '0.5rem' : '0',
 						}}>
 						<p>{message.username}</p>
 						<p className='font-bold'>{decryptMessage(message.content, groupKey)}</p>
